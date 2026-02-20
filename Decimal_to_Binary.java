2c63ec9ced723ffc3b05dae788ab40d97da6524d
Bandhan kumar Das <165545683+bandhandas@users.noreply.github.com>
2026-02-20 10:31:58 +0530

    Create Decimal_to_Binary.java

---
+++ Decimal_to_Binary.java
@@ -0,0 +1,20 @@
+public class DEC_TO_BIN {
+    public static void dectobin(int n){
+        int mynum= n;
+        int pow = 0;
+        int binnum=0;
+        while(n > 0){
+            int rem= n% 2;
+            binnum = binnum +(rem * (int)Math.pow(10  , pow ));
+            pow++;
+            n=n/2;
+        }
+        System.out.println("binany of "+ mynum + "  is "+ binnum);
+
+
+        }
+
+    public static void main(String[] args) {
+        dectobin(7);
+    }
+}
